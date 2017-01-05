5fa30de667e064d3b2e7d26d24d2f483330e9414
Pogorelyy <pogorely@amazon.com>
2017-01-05 09:47:31 -0800

    Lecture 49: Set default message for BookDetail

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 export default function () {
     return [
-        { title: 'Javascript: The Good Parts '},
-        { title: 'Harry Potter' },
-        { title: 'The Dark Tower' },
-        { title: 'Bible' },
-        { title: 'Trump: How to become great' }
+        { title: 'Javascript: The Good Parts', pages: 101 },
+        { title: 'Harry Potter', pages: 300 },
+        { title: 'The Dark Tower', pages: 450 },
+        { title: 'Bible', pages: 703 },
+        { title: 'Trump: How to become great', pages: 5 }
     ]
 }
