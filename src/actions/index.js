5cc4b73d36c5ecfbaf77bd5c05aac9fb62f3e228
Andrey Pogorelyy <andrey.pogorelyy@gmail.com>
2017-01-09 10:58:02 -0800

    Lecture 57: added a request to get weather data

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
+import axios from 'axios';
+
 const API_KEY = 'a361e7540c76df8d196f60e88dfe5a90';
+
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);  // returns a promise
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
