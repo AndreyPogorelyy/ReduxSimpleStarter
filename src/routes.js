cb43f804d75a5296f32fd25ee1596b52adbcfad0
Andrey Pogorelyy <andrey.pogorelyy@gmail.com>
2017-01-13 12:17:45 -0800

    Lecture 75: added temp child routes

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,14 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
 
+const Greeting = () => {
+    return <div>Hey Ho!</div>;
+}
+
 export default (
-    <Route path="/" component={App} />
+    <Route path="/" component={App}>
+        <Route path="greet" component={Greeting} />
+        <Route path="smile" component={Greeting} />
+        <Route path="fun" component={Greeting} />
+    </Route>
 );
