3f520efcfacd8a5bf1ffedd088697aa703706f0e
Andrey Pogorelyy <andrey.pogorelyy@gmail.com>
2017-01-18 20:20:53 -0800

    Lecture 89: expanded form validation

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,30 +1,43 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostNew extends Component {
+
     render() {
         const { fields: { title, categories, content }, handleSubmit } = this.props;
+
+        function createFormClass(field) {
+            return `form-group ${field.touched && field.invalid ? 'has-danger' : '' }`;
+        };
 
         return (
             <form onSubmit={handleSubmit(this.props.createPost)}>
                 <h3>Create a new post</h3>
-                <div className="form-group">
+                <div className={ createFormClass(title) }>
                     <label>Title</label>
                     <input type="text" className="form-control" {...title} />
                     <div className="text-help">
                         {title.touched ? title.error : ''}
                     </div>
                 </div>
-                <div className="form-group">
+                <div className={ createFormClass(categories) }>
                     <label>Categories</label>
                     <input type="text" className="form-control" {...categories} />
+                    <div className="text-help">
+                        {categories.touched ? categories.error : ''}
+                    </div>
                 </div>
-                <div className="form-group">
+                <div className={ createFormClass(content) }>
                     <label>Content</label>
                     <textarea className="form-control" {...content} />
+                    <div className="text-help">
+                        {content.touched ? content.error : ''}
+                    </div>
                 </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
+                <button type="submit" className="btn btn-primary">Save</button>
+                <Link to="/" className="btn btn-danger">Cancel</Link>
             </form>
         );
     }
@@ -36,6 +49,15 @@ function validate(values) {
     if (!values.title) {
         errors.title = "Enter a username";
     }
+
+    if (!values.categories) {
+        errors.categories = "Enter categories";
+    }
+
+    if (!values.content) {
+        errors.content = "Enter some content";
+    }
+
 
     return errors;
 }
