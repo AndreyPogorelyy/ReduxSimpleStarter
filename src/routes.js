fe0475de833adebb90d825b1f0bb352b102dfacc
Andrey Pogorelyy <andrey.pogorelyy@gmail.com>
2017-01-20 15:56:02 -0800

    Lecture 93: Added PostsShow

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,12 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import PostsIndex from './components/posts_index';
 import PostNew from './components/post_new';
+import PostsShow from './components/posts_show';
 
 export default (
     <Route path="/" component={App}>
         <IndexRoute component={PostsIndex} />
         <Route path="post/new" component={PostNew} />
+        <Route path="posts/:id" component={PostsShow} />
     </Route>
 );
