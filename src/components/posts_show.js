62e990fc10e82e533229a68426ffd5f19abfa9d4
Andrey Pogorelyy <andrey.pogorelyy@gmail.com>
2017-01-22 21:11:50 -0800

    Lecture 94: delete post

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchPost } from '../actions/index';
+import { fetchPost, deletePost } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsShow extends Component {
     componentWillMount() {
         this.props.fetchPost(this.props.params.id);
     }
 
+    onDeleteClick() {
+        this.props.deletePost(this.props.params.id);
+    }
+
     render() {
-        console.log(this.props.post);
         const {post } = this.props;
 
         if (!this.props.post) {
@@ -19,6 +23,12 @@ class PostsShow extends Component {
 
         return (
             <div> 
+                <Link to="/">Back to posts</Link>
+                <button 
+                    onClick={this.onDeleteClick.bind(this)}
+                    className="btn btn-danger pull-xs-right">
+                    Delete post
+                </button>
                 <h3>{post.title}</h3>
                 <h6>Categories {post.categories}</h6>
                 <p>{post.content}</p>
@@ -31,4 +41,4 @@ function mapStateToProps(state) {
     return { post: state.posts.post };
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
